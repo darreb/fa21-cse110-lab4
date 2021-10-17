cb4280856c3cbb299c0ecc0f7bfb31b2bda4386c
Darren Cheng <38565956+darreb@users.noreply.github.com>
2021-10-16 18:12:48 -0700

    Add files via upload

---
+++ expose/javascript/part2-question18.js
@@ -0,0 +1,3 @@
+let intervalID = setInterval(()=>{let d = new Date();
+                                  let time = d.toLocaleTimeString();
+                                  console.log(time);}, 1000);
